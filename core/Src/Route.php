7279cac510fb51cc9c1ba1ecdc16feb595c918c1
Александр <soedinennye@gmail.com>
2025-05-01 00:29:02 +0700

    Change Route.php

--- core/Src/Route.php
+++ core/Src/Route.php
@@ -3,7 +3,6 @@
 namespace Src;
 
 use Error;
-
 use FastRoute\RouteCollector;
 use FastRoute\RouteParser\Std;
 use FastRoute\DataGenerator\MarkBased;
@@ -12,20 +11,20 @@ use Src\Traits\SingletonTrait;
 
 class Route
 {
-    //Используем методы трейта
+    // Используем методы трейта SingletonTrait
     use SingletonTrait;
 
-    //Свойство для хранения текущего маршрута
+    // Свойство для хранения текущего маршрута
     private string $currentRoute = '';
     private $currentHttpMethod;
 
-    //Свойство для префикса для всех маршрутов
+    // Свойство для префикса для всех маршрутов
     private string $prefix = '';
 
-    //Классы для использования внешнего маршрутизатора
+    // Класс для использования внешнего маршрутизатора
     private RouteCollector $routeCollector;
 
-    //Добавляет маршрут, устанавливает его текущим и возвращает объект
+    // Добавляет маршрут, устанавливает его текущим и возвращает объект
     public static function add($httpMethod, string $route, array $action): self
     {
         self::single()->routeCollector->addRoute($httpMethod, $route, $action);
@@ -34,14 +33,14 @@ class Route
         return self::single();
     }
 
-    //Добавляет префикс для обозначенных маршрутов
+    // Добавляет префикс для групп маршрутов
     public static function group(string $prefix, callable $callback): void
     {
         self::single()->routeCollector->addGroup($prefix, $callback);
         Middleware::single()->group($prefix, $callback);
     }
 
-    //Конструктор скрыт. Вызывается только один раз
+    // Конструктор скрыт. Вызывается только один раз.
     private function __construct()
     {
         $this->routeCollector = new RouteCollector(new Std(), new MarkBased());
@@ -63,29 +62,43 @@ class Route
         return $this->prefix . $url;
     }
 
-    //Добавление middlewares для текущего маршрута
+    // Добавление middleware для текущего маршрута
     public function middleware(...$middlewares): self
     {
         Middleware::single()->add($this->currentHttpMethod, $this->currentRoute, $middlewares);
-        return $this;
+        return self::single();
     }
 
     public function start(): void
     {
-        // Fetch method and URI from somewhere
+        // Получаем метод и URI из серверных переменных
         $httpMethod = $_SERVER['REQUEST_METHOD'];
         $uri = $_SERVER['REQUEST_URI'];
 
-        // Strip query string (?foo=bar) and decode URI
+        // Отсекаем query-string, если он присутствует, и декодируем URI
         if (false !== $pos = strpos($uri, '?')) {
             $uri = substr($uri, 0, $pos);
         }
         $uri = rawurldecode($uri);
+
+        // Отсекаем префикс из URI
         $uri = substr($uri, strlen($this->prefix));
 
-        $dispatcher = new Dispatcher($this->routeCollector->getData());
+        // Если URI пустой, задаём базовый путь "/"
+        if ($uri === '') {
+            $uri = '/';
+        }
 
+        // **** Логирование для отладки ****
+        error_log("HTTP Method: $httpMethod");
+        error_log("Original URI: " . $_SERVER['REQUEST_URI']);
+        error_log("Prefix: " . $this->prefix);
+        error_log("Processed URI: " . $uri);
+        // ***********************************
+
+        $dispatcher = new Dispatcher($this->routeCollector->getData());
         $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
         switch ($routeInfo[0]) {
             case Dispatcher::NOT_FOUND:
                 throw new Error('NOT_FOUND');
@@ -94,9 +107,15 @@ class Route
             case Dispatcher::FOUND:
                 $handler = $routeInfo[1];
                 $vars = array_values($routeInfo[2]);
+
+                error_log("Route FOUND - Handler: " . print_r($handler, true));
+                error_log("Route FOUND - Vars: " . print_r($vars, true));
+
+                // Запускаем middleware для данного маршрута
                 $vars[] = Middleware::single()->runMiddlewares($httpMethod, $uri);
                 $class = $handler[0];
                 $action = $handler[1];
+
                 call_user_func([new $class, $action], ...$vars);
                 break;
         }
